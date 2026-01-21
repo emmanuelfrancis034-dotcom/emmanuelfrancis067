aa7ba0c0cb99101f53931493522cb7fbd910444b
emmanuelfrancis034-dotcom <emmanuelfrancis034@gmail.com>
2026-01-21 19:45:44 +0530

    main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('Muaaaaahhh...').split('')
+    const titles = ('Muaaaaah').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
